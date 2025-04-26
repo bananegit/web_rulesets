c9d8e156f4a0fa6ceff344be95fa45ace892bb96
bananegit <redbaron90@gmail.com>
2025-04-26 13:50:53 +0200

    Update UnitTest1.cs

--- Artificial Intelligence/Tests/DeepSeekTests/UnitTest1.cs
+++ Artificial Intelligence/Tests/DeepSeekTests/UnitTest1.cs
@@ -21,7 +21,12 @@ namespace DeepSeekTests
         [Test]
         public async Task Test1()
         {
-            using (var driver = UndetectedChromeDriver.Create(driverExecutablePath: await new ChromeDriverInstaller().Auto()))
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--no-sandbox");
+            options.AddArguments("--disable-dev-shm-usage");
+            options.AddArguments("--headless=new");
+            options.AddArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0");
+            using (var driver = UndetectedChromeDriver.Create(options, driverExecutablePath: await new ChromeDriverInstaller().Auto()))
                 {
 
                 driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(10000);
@@ -29,7 +34,7 @@ namespace DeepSeekTests
                 driver.Navigate().GoToUrl(baseUrl);
 
                 var inputs = driver.FindElements(By.ClassName("ds-input__input"));
-
+                
                 foreach (var input in inputs)
                 {
                     if (input.GetAttribute("type") == "text")
@@ -41,15 +46,25 @@ namespace DeepSeekTests
                         input.SendKeys(password);
                     }
                 }
-                var loginButton = driver.FindElement(By.ClassName("ds-sign-up-form__register-button"));
-                loginButton.Click();
-                //await Task.Delay(5000);
+                
+                try
+                {
+                    var loginButton = driver.FindElement(By.ClassName("ds-sign-up-form__register-button"));
+                    loginButton.Click();
 
-                TextCopy.ClipboardService.SetText(shortTestString);
-                var textArea = driver.FindElement(By.Id(inputId));
-                textArea.SendKeys(Keys.Control + "v");
+                    //TextCopy.ClipboardService.SetText(shortTestString);
+                    driver.ExecuteScript("navigator.clipboard.writeText(\"" + shortTestString + "\");");
+                    var textArea = driver.FindElement(By.Id(inputId));
+                    textArea.SendKeys(Keys.Control + "v");
 
-                Assert.That(textArea.Text, Is.EqualTo(shortTestString));
+                    Assert.That(textArea.Text, Is.EqualTo(shortTestString));
+                }
+                catch (Exception)
+                {
+                    var ss = driver.GetScreenshot();
+                    Console.WriteLine(ss.AsBase64EncodedString);
+                    throw;
+                }
             }
 
             //driver.Quit();
