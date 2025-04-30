511e8c967804e4bfa2b4e0aafd21baf408667e05
bananegit <redbaron90@gmail.com>
2025-04-30 03:17:40 +0200

    Update UnitTest1.cs

--- Artificial Intelligence/Tests/DeepSeekTests/UnitTest1.cs
+++ Artificial Intelligence/Tests/DeepSeekTests/UnitTest1.cs
@@ -59,7 +59,7 @@ namespace DeepSeekTests
             prefs = new Dictionary<string, object>();
             options.AddArguments("--no-sandbox");
             options.AddArguments("--disable-dev-shm-usage");
-            //options.AddArguments("--headless=new");
+            options.AddArguments("--headless=new");
             options.AddArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0");
             var t = (DateTime.UtcNow - new DateTime(1970, 1, 1));
             
