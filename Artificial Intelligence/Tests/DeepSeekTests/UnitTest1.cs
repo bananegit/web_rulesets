cb650b1740642934360bea866ae07419f67062db
bananegit <redbaron90@gmail.com>
2025-05-01 03:14:38 +0200

    Update UnitTest1.cs

--- Artificial Intelligence/Tests/DeepSeekTests/UnitTest1.cs
+++ Artificial Intelligence/Tests/DeepSeekTests/UnitTest1.cs
@@ -29,18 +29,21 @@ namespace DeepSeekTests
         private String uploadErrorContainerCn = "_5119742";
         private String thinkContainerCN = "_19db599";
         private String inputId = "chat-input";
+        private int executionCounter = 0;
+        private int retries = 3;
 
 
         [OneTimeSetUp]
         public async Task oneTimeInit()
         {
-            string traceId = "9b3d8915";
+            string traceId = "";
             await startTrace(traceId);
         }
 
         [SetUp]
         public void Setup()
         {
+            executionCounter = 0;
             options = new ChromeOptions();
             prefs = new Dictionary<string, object>();
             options.AddArguments("--no-sandbox");
@@ -52,167 +55,36 @@ namespace DeepSeekTests
             prefs.Add("profile.content_settings.exceptions.clipboard", new Dictionary<string, object>() { { "[*.]deepseek.com,*", new Dictionary<string, object> { { "last_modfied", (int)t.TotalSeconds * 1000 }, { "setting", 1 } } } });
         }
 
-        [TestCase(null, TestName ="Enforce DeepThink Setting (WebApp)")]
-        public async Task EnforceDeepThinkSettingsTest(object? param)
+        private void SetupNoReset()
         {
+            options = new ChromeOptions();
+            prefs = new Dictionary<string, object>();
+            options.AddArguments("--no-sandbox");
+            options.AddArguments("--disable-dev-shm-usage");
+            options.AddArguments("--headless=new");
+            options.AddArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0");
+            var t = (DateTime.UtcNow - new DateTime(1970, 1, 1));
 
-            using (var driver = UndetectedChromeDriver.Create(options, commandTimeout:commandTimeout, prefs: prefs, driverExecutablePath: await new ChromeDriverInstaller().Auto()))
-            {
-                try
-                {
-                    authenticate(driver);
-
-                    var textArea = driver.FindElement(By.Id(inputId));
-                    textArea.SendKeys(searchPrompt);
-                    var sendButton = driver.FindElement(By.ClassName(sendPromptButtonCn));
-                    sendButton.Click();
-
-                    var reply = driver.FindElement(By.ClassName(replyContainerCn));
-                    var thinkContainer = driver.FindElement(By.ClassName(thinkContainerCN));
-
-                    StringAssert.IsMatch("Thought for [0-9]* seconds", thinkContainer.Text);
-                }
-                catch (Exception)
-                {
-                    var ss = driver.GetScreenshot();
-                    Console.WriteLine("Error Screenshot: \r\n" + ss.AsBase64EncodedString);
-                    throw;
-                }
-                finally
-                {
-                    driver.Quit();
-                }
-            }
+            prefs.Add("profile.content_settings.exceptions.clipboard", new Dictionary<string, object>() { { "[*.]deepseek.com,*", new Dictionary<string, object> { { "last_modfied", (int)t.TotalSeconds * 1000 }, { "setting", 1 } } } });
         }
 
-        [TestCase(null, TestName = "Enforce Search Setting (WebApp)")]
-        public async Task EnforceSearchSettingsTest(object? param)
+        private void restoreSCPPolicy()
         {
-
-            using (var driver = UndetectedChromeDriver.Create(options, commandTimeout: commandTimeout, prefs: prefs, driverExecutablePath: await new ChromeDriverInstaller().Auto()))
-            {
-                try
-                {
-                    authenticate(driver);
-
-                    var textArea = driver.FindElement(By.Id(inputId));
-                    textArea.SendKeys(searchPrompt);
-                    var sendButton = driver.FindElement(By.ClassName(sendPromptButtonCn));
-                    sendButton.Click();
-
-                    var reply = driver.FindElement(By.ClassName(replyContainerCn));
-
-                    StringAssert.IsMatch(".*(can't|cannot|can not).*search.*", reply.Text);
-                }
-                catch (Exception)
-                {
-                    var ss = driver.GetScreenshot();
-                    Console.WriteLine("Error Screenshot: \r\n" + ss.AsBase64EncodedString);
-                    throw;
-                }
-                finally
-                {
-                    driver.Quit();
-                }
-            }
+            Console.WriteLine("restoring scp policy");
+            var usPolicyFile = AppContext.BaseDirectory + "ScpPolicy.opg";
+            File.Copy(usPolicyFile, "C:\\ProgramData\\Skyhigh\\SCP\\Policy\\Temp\\ScpPolicy.opg", true);
         }
 
-        [TestCase(null, TestName = "Prevent pasting from clipboard (WebApp)")]
-        public async Task PreventPastingFromClipBoardTest(object? param)
+        private void overWriteSCPPolicy()
         {
-            
-            using (var driver = UndetectedChromeDriver.Create(options, commandTimeout: commandTimeout, prefs: prefs, driverExecutablePath: await new ChromeDriverInstaller().Auto()))
-                {
-                    try
-                    {
-                    authenticate(driver);
-                    
-                        driver.ExecuteScript("navigator.clipboard.writeText(\"" + longTestString + "\");");
-                        var clipboardContent = driver.ExecuteScript("return await navigator.clipboard.readText();");
-                    
-                        var textArea = driver.FindElement(By.Id(inputId));
-                        textArea.SendKeys(Keys.Control + "v");
-
-                        Assert.That(clipboardContent, Is.EqualTo(longTestString));
-                        Assert.That(textArea.Text, Is.EqualTo(String.Empty));
-                    }
-                    catch (Exception)
-                    {
-                        var ss = driver.GetScreenshot();
-                        Console.WriteLine("Error Screenshot: \r\n" + ss.AsBase64EncodedString);
-                        throw;
-                }
-                finally
-                {
-                    driver.Quit();
-                }
-            }
-        }
-
-        [TestCase(null, TestName = "Block File Upload (WebApp)")]
-        public async Task BlockFileUploadsTest(object? param)
-        {
-
-            using (var driver = UndetectedChromeDriver.Create(options, commandTimeout: commandTimeout, prefs: prefs, driverExecutablePath: await new ChromeDriverInstaller().Auto()))
-            {
-                try
-                {
-                    authenticate(driver);
-
-                    var fileInput = driver.FindElement(By.CssSelector("input[type=file]"));
-                    fileInput.SendKeys(bitmapFilePath);
-                    await Task.Delay(5000);
-                    var errorElement = driver.FindElement(By.ClassName(uploadErrorContainerCn));
-
-                    Assert.That(errorElement.Text, Is.EqualTo("Upload failed"));
-                }
-                catch (Exception)
-                {
-                    var ss = driver.GetScreenshot();
-                    Console.WriteLine("Error Screenshot: \r\n" + ss.AsBase64EncodedString);
-                    throw;
-                }
-                finally
-                {
-                    driver.Quit();
-                }
-            }
-        }
-
-        [TestCase(null, TestName = "Replace prompt (WebApp)")]
-        public async Task ReplacePromptTest(object? param)
-        {
-
-            using (var driver = UndetectedChromeDriver.Create(options, commandTimeout: commandTimeout, prefs: prefs, driverExecutablePath: await new ChromeDriverInstaller().Auto()))
-            {
-                try
-                {
-                    authenticate(driver);
-
-                    var textArea = driver.FindElement(By.Id(inputId));
-                    textArea.SendKeys(longTestString);
-                    var sendButton = driver.FindElement(By.ClassName(sendPromptButtonCn));
-                    sendButton.Click();
-
-                    var reply = driver.FindElement(By.ClassName(replyContainerCn));
-                    Assert.That(reply.Text, Is.EqualTo(policyWarning));
-                }
-                catch (Exception)
-                {
-                    var ss = driver.GetScreenshot();
-                    Console.WriteLine("Error Screenshot: \r\n" + ss.AsBase64EncodedString);
-                    throw;
-                }
-                finally
-                {
-                    driver.Quit();
-                }
-            }
+            Console.WriteLine("overwriting scp policy with us policy");
+            var usPolicyFile = AppContext.BaseDirectory + "usPolicy.opg";
+            File.Copy(usPolicyFile, "C:\\ProgramData\\Skyhigh\\SCP\\Policy\\Temp\\ScpPolicy.opg", true);
         }
 
         private void authenticate(UndetectedChromeDriver? driver)
         {
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500000);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(150000);
 
             driver.Navigate().GoToUrl(baseUrl);
 
@@ -255,6 +127,208 @@ namespace DeepSeekTests
             }
         }
 
+        [TestCase(null, TestName ="Enforce DeepThink Setting (WebApp)")]
+        public async Task EnforceDeepThinkSettingsTest(object? param)
+        {
+            while (executionCounter < retries)
+            {
+                using (var driver = UndetectedChromeDriver.Create(options, commandTimeout: commandTimeout, prefs: prefs, driverExecutablePath: await new ChromeDriverInstaller().Auto()))
+                {
+                    try
+                    {
+                        authenticate(driver);
+
+                        var textArea = driver.FindElement(By.Id(inputId));
+                        textArea.SendKeys(searchPrompt);
+                        var sendButton = driver.FindElement(By.ClassName(sendPromptButtonCn));
+                        sendButton.Click();
+
+                        var reply = driver.FindElement(By.ClassName(replyContainerCn));
+                        var thinkContainer = driver.FindElement(By.ClassName(thinkContainerCN));
+
+                        StringAssert.IsMatch("Thought for [0-9]* seconds", thinkContainer.Text);
+                        return;
+                    }
+                    catch (Exception)
+                    {
+                        var ss = driver.GetScreenshot();
+                        Console.WriteLine("Error Screenshot: \r\n" + ss.AsBase64EncodedString);
+                        if (executionCounter == retries)
+                        {
+                            throw;
+                        }
+                    }
+                    finally
+                    {
+                        driver.Quit();
+                    }
+                }
+                executionCounter++;
+                SetupNoReset();
+            }
+        }
+
+        [TestCase(null, TestName = "Enforce Search Setting (WebApp)")]
+        public async Task EnforceSearchSettingsTest(object? param)
+        {
+            while(executionCounter < retries)
+            {
+                using (var driver = UndetectedChromeDriver.Create(options, commandTimeout: commandTimeout, prefs: prefs, driverExecutablePath: await new ChromeDriverInstaller().Auto()))
+                {
+                    try
+                    {
+                        authenticate(driver);
+
+                        //TODO actually enable search in UI
+                        var textArea = driver.FindElement(By.Id(inputId));
+                        textArea.SendKeys(searchPrompt);
+                        var sendButton = driver.FindElement(By.ClassName(sendPromptButtonCn));
+                        sendButton.Click();
+
+                        var reply = driver.FindElement(By.ClassName(replyContainerCn));
+
+                        StringAssert.IsMatch(".*(can't|cannot|can not).*search.*", reply.Text);
+                        return;
+                    }
+                    catch (Exception)
+                    {
+                        var ss = driver.GetScreenshot();
+                        Console.WriteLine("Error Screenshot: \r\n" + ss.AsBase64EncodedString);
+                        if(executionCounter == retries)
+                        {
+                            throw;
+                        }
+                    }
+                    finally
+                    {
+                        driver.Quit();
+                    }
+                }
+                executionCounter++;
+                SetupNoReset();
+            }
+            
+        }
+
+        [TestCase(null, TestName = "Prevent pasting from clipboard (WebApp)")]
+        public async Task PreventPastingFromClipBoardTest(object? param)
+        {
+            while(executionCounter < retries)
+            {
+                using (var driver = UndetectedChromeDriver.Create(options, commandTimeout: commandTimeout, prefs: prefs, driverExecutablePath: await new ChromeDriverInstaller().Auto()))
+                {
+                    try
+                    {
+                        authenticate(driver);
+
+                        driver.ExecuteScript("navigator.clipboard.writeText(\"" + longTestString + "\");");
+                        var clipboardContent = driver.ExecuteScript("return await navigator.clipboard.readText();");
+
+                        var textArea = driver.FindElement(By.Id(inputId));
+                        textArea.SendKeys(Keys.Control + "v");
+
+                        Assert.That(clipboardContent, Is.EqualTo(longTestString));
+                        Assert.That(textArea.Text, Is.EqualTo(String.Empty));
+                        return;
+                    }
+                    catch (Exception)
+                    {
+                        var ss = driver.GetScreenshot();
+                        Console.WriteLine("Error Screenshot: \r\n" + ss.AsBase64EncodedString);
+                        if(executionCounter == retries)
+                        {
+                            throw;
+                        }
+                    }
+                    finally
+                    {
+                        driver.Quit();
+                    }
+                }
+                executionCounter++;
+                SetupNoReset();
+            }
+            
+        }
+
+        [TestCase(null, TestName = "Block File Upload (WebApp)")]
+        public async Task BlockFileUploadsTest(object? param)
+        {
+            while (executionCounter < retries)
+            {
+                using (var driver = UndetectedChromeDriver.Create(options, commandTimeout: commandTimeout, prefs: prefs, driverExecutablePath: await new ChromeDriverInstaller().Auto()))
+                {
+                    try
+                    {
+                        authenticate(driver);
+
+                        var fileInput = driver.FindElement(By.CssSelector("input[type=file]"));
+                        fileInput.SendKeys(bitmapFilePath);
+                        await Task.Delay(5000);
+                        var errorElement = driver.FindElement(By.ClassName(uploadErrorContainerCn));
+
+                        Assert.That(errorElement.Text, Is.EqualTo("Upload failed"));
+                        return;
+                    }
+                    catch (Exception)
+                    {
+                        var ss = driver.GetScreenshot();
+                        Console.WriteLine("Error Screenshot: \r\n" + ss.AsBase64EncodedString);
+                        if (executionCounter == retries) 
+                        {  
+                            throw; 
+                        }
+                    }
+                    finally
+                    {
+                        driver.Quit();
+                    }
+                }
+                executionCounter++;
+                SetupNoReset();
+            }
+            
+        }
+
+        [TestCase(null, TestName = "Replace prompt (WebApp)")]
+        public async Task ReplacePromptTest(object? param)
+        {
+            while (executionCounter < retries)
+            {
+                using (var driver = UndetectedChromeDriver.Create(options, commandTimeout: commandTimeout, prefs: prefs, driverExecutablePath: await new ChromeDriverInstaller().Auto()))
+                {
+                    try
+                    {
+                        authenticate(driver);
+
+                        var textArea = driver.FindElement(By.Id(inputId));
+                        textArea.SendKeys(longTestString);
+                        var sendButton = driver.FindElement(By.ClassName(sendPromptButtonCn));
+                        sendButton.Click();
+
+                        var reply = driver.FindElement(By.ClassName(replyContainerCn));
+                        Assert.That(reply.Text, Is.EqualTo(policyWarning));
+                        return;
+                    }
+                    catch (Exception)
+                    {
+                        var ss = driver.GetScreenshot();
+                        Console.WriteLine("Error Screenshot: \r\n" + ss.AsBase64EncodedString);
+                        if(executionCounter == retries)
+                        {
+                            throw;
+                        }
+                    }
+                    finally
+                    {
+                        driver.Quit();
+                    }
+                }
+                executionCounter++;
+                SetupNoReset();
+            }
+        }
+
         //greenfield tests
         //this test verifies that the way we copy and paste data into the site would work under normal circumstances
         [TestCase(null, TestName = "_greenField_Prevent pasting from clipboard (WebApp)")]
@@ -293,26 +367,11 @@ namespace DeepSeekTests
                     driver.Quit();
                     if (!String.IsNullOrEmpty(ci))
                     {
-                        await restoreSCPPolicy();
+                        restoreSCPPolicy();
                     }
                         
                 }
             }
-        }
-
-        private async Task restoreSCPPolicy()
-        {
-            Console.WriteLine("restoring scp policy");
-            var usPolicyFile = AppContext.BaseDirectory + "ScpPolicy.opg";
-            File.Copy(usPolicyFile, "C:\\ProgramData\\Skyhigh\\SCP\\Policy\\Temp\\ScpPolicy.opg", true);
-            await Task.Delay(10000);
-        }
-
-        private void overWriteSCPPolicy()
-        {
-            Console.WriteLine("overwriting scp policy with us policy");
-            var usPolicyFile = AppContext.BaseDirectory + "usPolicy.opg";
-            File.Copy(usPolicyFile, "C:\\ProgramData\\Skyhigh\\SCP\\Policy\\Temp\\ScpPolicy.opg", true);
         }
     }
 }
